47441d9a3cf81ae9f7324b14e455824ffce88e74
Rafael <akafael@users.noreply.github.com>
2021-08-29 15:41:09 -0300

    Update Songs

--- src/music/music.ino
+++ src/music/music.ino
@@ -1,10 +1,9 @@
 /**
- * 
+ * Play Songs
  * 
  * @author akafael
  * @ref https://github.com/akafael/arduino-music
  */
-
 
 #define TONE_USE_INT
 #define TONE_PITCH 440
@@ -12,34 +11,56 @@
 
 #include "musics.h"
 
-// Two things need to be created: the array for the notes of the melody (in order)
-// and the duration of each (think of it like sheet music in two parts)
+const int buzzer_pin = 8;
 
-// BOTH ARRAYS MUST BE THE SAME SIZE!
+//const int tempo = ASA_BRANCA_TEMPO;
+//const int melody[] = ASA_BRANCA_MELODY;
+const int tempo = PULO_DA_GAITA_TEMPO;
+const int melody[] = PULO_DA_GAITA_MELODY;
 
-const int melody[] = AHA_MELODY;
-const int durations[] = AHA_DURATION;
+// sizeof gives the number of bytes, each int value is composed of two bytes (16 bits)
+// there are two values per note (pitch and duration), so for each note there are four bytes
+const int notes = sizeof(melody) / sizeof(melody[0]) / 2;
 
-// determine the length of the arrays to use in the loop iteration
-const int songLength = sizeof(melody)/sizeof(melody[0]);
-const int durationsLength = sizeof(durations)/sizeof(durations[0]);
+// this calculates the duration of a whole note in ms
+const int noteTempo = (60000 * 4) / tempo;
+
 int thisNote = 0;
 
 void setup() {
- //We don't need anything here
+  // do nothing
 }
 
 void loop() {
+  playMusic( melody, noteTempo );
+}
 
-  // determine the duration of the notes that the computer understands
-  // divide 1000 by the value, so the first note lasts for 1000/8 milliseconds
-  thisNote = (thisNote < songLength)? thisNote+1 : 0;
-  int duration = 1000/ durations[thisNote];
-  tone(8, melody[thisNote], duration);
-  // pause between notes
-  int pause = duration * 1.3;
-  delay(pause);
-  // stop the tone
-  noTone(8);
+/**
+ * Play Music Score
+ * @author 
+ */
+void playMusic( const int *melodySong, const int wholenote )
+{
+  // iterate over the notes of the melody.
+  // Remember, the array is twice the number of notes (notes + durations)
+  thisNote = (thisNote < notes * 2)? thisNote + 2 : 0;
 
+  // calculates the duration of each note
+  const int divider = melody[thisNote + 1];
+
+  int noteDuration = (wholenote) / divider;
+
+  if (divider < 0) {
+    // dotted notes are represented with negative durations!!
+    noteDuration *= -1.5; // increases the duration in half for dotted notes
+  }
+
+  // we only play the note for 90% of the duration, leaving 10% as a pause
+  tone(buzzer_pin, melodySong[thisNote], noteDuration * 0.9);
+
+  // Wait for the specief duration before playing the next note.
+  delay(noteDuration);
+
+  // stop the waveform generation before the next note.
+  noTone(buzzer_pin);
 }
--- src/music/musics.h
+++ src/music/musics.h
@@ -1,5 +1,5 @@
 /**
- * 
+ * Arduino Songs Collection from @robsoncouto 
  * 
  * @author akafael
  * @ref https://github.com/akafael/arduino-music
@@ -10,19 +10,107 @@
 #define TONE_PITCH 440
 #include <Pitch.h> // Required for tone()
 
+#define REST 0
 
-// The melody array 
-#define AHA_MELODY { \
-  NOTE_FS5, NOTE_FS5, NOTE_D5, NOTE_B4, NOTE_B4, NOTE_E5, \
-  NOTE_E5, NOTE_E5, NOTE_GS5, NOTE_GS5, NOTE_A5, NOTE_B5, \
-  NOTE_A5, NOTE_A5, NOTE_A5, NOTE_E5, NOTE_D5, NOTE_FS5, \
-  NOTE_FS5, NOTE_FS5, NOTE_E5, NOTE_E5, NOTE_FS5, NOTE_E5\
-}
+// notes of the moledy followed by the duration.
+// a 4 means a quarter note, 8 an eighteenth , 16 sixteenth, so on
+// !!negative numbers are used to represent dotted notes,
+// so -4 means a dotted quarter note, that is, a quarter plus an eighteenth!!
 
-// The note duration, 8 = 8th note, 4 = quarter note, etc.
-#define AHA_DURATION { \
-  8, 8, 8, 4, 4, 4, \
-  4, 5, 8, 8, 8, 8, \
-  8, 8, 8, 4, 4, 4, \
-  4, 5, 8, 8, 8, 8 \
+/**
+ * Pulo da gaita - Auto da Compadecida 
+ *  - Score available at https://musescore.com/user/196039/scores/250206
+ *  - Arduino version at https://github.com/robsoncouto/arduino-songs/blob/master/pulodagaita/pulodagaita.ino
+ */
+#define PULO_DA_GAITA_MELODY { \
+  NOTE_C5,4, NOTE_G4,8, NOTE_AS4,4, NOTE_A4,8,\
+  NOTE_G4,16, NOTE_C4,8, NOTE_C4,16, NOTE_G4,16, NOTE_G4,8, NOTE_G4,16,\
+  NOTE_C5,4, NOTE_G4,8, NOTE_AS4,4, NOTE_A4,8,\
+  NOTE_G4,2,\
+\
+  NOTE_C5,4, NOTE_G4,8, NOTE_AS4,4, NOTE_A4,8,\
+  NOTE_G4,16, NOTE_C4,8, NOTE_C4,16, NOTE_G4,16, NOTE_G4,8, NOTE_G4,16,\
+  NOTE_F4,8, NOTE_E4,8, NOTE_D4,8, NOTE_C4,8,\
+  NOTE_C4,2,\
+\
+  NOTE_C5,4, NOTE_G4,8, NOTE_AS4,4, NOTE_A4,8,\
+  NOTE_G4,16, NOTE_C4,8, NOTE_C4,16, NOTE_G4,16, NOTE_G4,8, NOTE_G4,16,\
+  NOTE_C5,4, NOTE_G4,8, NOTE_AS4,4, NOTE_A4,8,\
+  NOTE_G4,2,\
+\
+  NOTE_C5,4, NOTE_G4,8, NOTE_AS4,4, NOTE_A4,8,\
+  NOTE_G4,16, NOTE_C4,8, NOTE_C4,16, NOTE_G4,16, NOTE_G4,8, NOTE_G4,16,\
+  NOTE_F4,8, NOTE_E4,8, NOTE_D4,8, NOTE_C4,8,\
+  NOTE_C4,16, NOTE_D5,8, NOTE_D5,16, NOTE_D5,16, NOTE_D5,8, NOTE_D5,16,\
+\
+  NOTE_D5,16, NOTE_D5,8, NOTE_D5,16, NOTE_C5,8, NOTE_E5,-8,\
+  NOTE_C5,8, NOTE_C5,16, NOTE_E5,16, NOTE_E5,8, NOTE_C5,16,\
+  NOTE_F5,8, NOTE_D5,8, NOTE_D5,8, NOTE_E5,-8,\
+  NOTE_C5,8, NOTE_D5,16, NOTE_E5,16, NOTE_D5,8, NOTE_C5,16,\
+\
+  NOTE_F5,8, NOTE_F5,8, NOTE_A5,8, NOTE_G5,-8,\
+  NOTE_G5,8, NOTE_C5,16, NOTE_C5,16, NOTE_C5,8, NOTE_C5,16,\
+  NOTE_F5,-8, NOTE_E5,16, NOTE_D5,8, NOTE_C5,4,\
+  NOTE_C5,16, NOTE_C5,16, NOTE_C5,16, NOTE_C5,16,\
+\
+  NOTE_F5,8, NOTE_F5,16, NOTE_A5,8, NOTE_G5,-8,\
+  NOTE_G5,8, NOTE_C5,16, NOTE_C5,16, NOTE_C5,8, NOTE_C5,16,\
+  NOTE_F5,16, NOTE_E5,8, NOTE_D5,16, NOTE_C5,8, NOTE_E5,-8,\
+  NOTE_C5,8, NOTE_D5,16, NOTE_E5,16, NOTE_D5,8, NOTE_C5,16,\
+\
+  NOTE_F5,8, NOTE_F5,16, NOTE_A5,8, NOTE_G5,-8,\
+  NOTE_G5,8, NOTE_C5,16, NOTE_C5,16, NOTE_C5,8, NOTE_C5,16,\
+  NOTE_F5,8, NOTE_E5,16, NOTE_D5,8, NOTE_C5,8,\
+  NOTE_C5,4, NOTE_G4,8, NOTE_AS4,4, NOTE_A4,8,\
+\
+  NOTE_G4,16, NOTE_C4,8, NOTE_C4,16, NOTE_G4,16, NOTE_G4,8, NOTE_G4,16,\
+  NOTE_C5,4, NOTE_G4,8, NOTE_AS4,4, NOTE_A4,8,\
+  NOTE_G4,2,\
+  NOTE_C5,4, NOTE_G4,8, NOTE_AS4,4, NOTE_A4,8,\
+\
+  NOTE_G4,16, NOTE_C4,8, NOTE_C4,16, NOTE_G4,16, NOTE_G4,8, NOTE_G4,16,\
+  NOTE_F4,8, NOTE_E4,8, NOTE_D4,8, NOTE_C4,-2,\
+  NOTE_C5,4, NOTE_G4,8, NOTE_AS4,4, NOTE_A4,8,\
+\
+  NOTE_G4,16, NOTE_C4,8, NOTE_C4,16, NOTE_G4,16, NOTE_G4,8, NOTE_G4,16,\
+  NOTE_C5,4, NOTE_G4,8, NOTE_AS4,4, NOTE_A4,8,\
+  NOTE_G4,2,\
+  NOTE_C5,4, NOTE_G4,8, NOTE_AS4,4, NOTE_A4,8,\
+\
+  NOTE_G4,16, NOTE_C4,8, NOTE_C4,16, NOTE_G4,16, NOTE_G4,8, NOTE_G4,16,\
+  NOTE_F4,8, NOTE_E4,8, NOTE_D4,8, NOTE_C4,-2,\
+  NOTE_C4,16, NOTE_C4,8, NOTE_C4,16, NOTE_E4,16, NOTE_E4,8, NOTE_E4,16,\
+  NOTE_F4,16, NOTE_F4,8, NOTE_F4,16, NOTE_FS4,16, NOTE_FS4,8, NOTE_FS4,16,\
+\
+  NOTE_G4,8, REST,8, NOTE_AS4,8, NOTE_C5,1,\
 }
+#define PULO_DA_GAITA_TEMPO 122
+
+/**
+ * Asa branca - Luiz Gonzaga
+ * Score available at https://musescore.com/user/190926/scores/181370
+ * Arduino Version available at https://github.com/robsoncouto/arduino-songs/blob/master/asabranca/asabranca.ino
+ */
+#define ASA_BRANCA_MELODY {\
+  NOTE_G4,8, NOTE_A4,8, NOTE_B4,4, NOTE_D5,4, NOTE_D5,4, NOTE_B4,4,\
+  NOTE_C5,4, NOTE_C5,2, NOTE_G4,8, NOTE_A4,8,\
+  NOTE_B4,4, NOTE_D5,4, NOTE_D5,4, NOTE_C5,4,\
+\
+  NOTE_B4,2, REST,8, NOTE_G4,8, NOTE_G4,8, NOTE_A4,8,\
+  NOTE_B4,4, NOTE_D5,4, REST,8, NOTE_D5,8, NOTE_C5,8, NOTE_B4,8,\
+  NOTE_G4,4, NOTE_C5,4, REST,8, NOTE_C5,8, NOTE_B4,8, NOTE_A4,8,\
+\
+  NOTE_A4,4, NOTE_B4,4, REST,8, NOTE_B4,8, NOTE_A4,8, NOTE_G4,8,\
+  NOTE_G4,2, REST,8, NOTE_G4,8, NOTE_G4,8, NOTE_A4,8,\
+  NOTE_B4,4, NOTE_D5,4, REST,8, NOTE_D5,8, NOTE_C5,8, NOTE_B4,8,\
+\
+  NOTE_G4,4, NOTE_C5,4, REST,8, NOTE_C5,8, NOTE_B4,8, NOTE_A4,8,\
+  NOTE_A4,4, NOTE_B4,4, REST,8, NOTE_B4,8, NOTE_A4,8, NOTE_G4,8,\
+  NOTE_G4,4, NOTE_F5,8, NOTE_D5,8, NOTE_E5,8, NOTE_C5,8, NOTE_D5,8, NOTE_B4,8,\
+\
+  NOTE_C5,8, NOTE_A4,8, NOTE_B4,8, NOTE_G4,8, NOTE_A4,8, NOTE_G4,8, NOTE_E4,8, NOTE_G4,8,\
+  NOTE_G4,4, NOTE_F5,8, NOTE_D5,8, NOTE_E5,8, NOTE_C5,8, NOTE_D5,8, NOTE_B4,8,\
+  NOTE_C5,8, NOTE_A4,8, NOTE_B4,8, NOTE_G4,8, NOTE_A4,8, NOTE_G4,8, NOTE_E4,8, NOTE_G4,8,\
+  NOTE_G4,-2, REST,4\
+}
+#define ASA_BRANCA_TEMPO 120
